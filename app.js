80715a5a59201a13ee0ee00f7d212426abb74c73
Zufeng W <ZufengW@gmail.com>
2017-10-30 20:00:40 +1100

    Add ejs, home page, css file

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 
+// tells express to serve contents of public directory
+app.use(express.static("public"));
+// tells express to expect ejs files. No longer need to put .ejs in filenames
+app.set("view engine", "ejs");
+
 app.get("/speak", function(req, res){
   console.log("wolo");
   res.send("/speak");
 });
 
+// Routes
+
 app.get("/", function(req, res){
-  res.send("root");
+  res.render("home");
 });
 
 
